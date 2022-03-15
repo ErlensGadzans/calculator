77b82ebfd72a8a9552b11ab9637dfb3d3a3f2bc2
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-03-15 14:13:45 +0200

    Created layout

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculator</title>
+</head>
+
+<body>
+
+    <div class="result"></div>
+    <div class="calc_grid">
+        <a href="?btn=7">7</a>
+        <a href="?btn=8">8</a>
+        <a href="?btn=9">9</a>
+        <a href="?btn=reset">AC</a>
+
+
+        <a href="?btn=4">4</a>
+        <a href="?btn=5">5</a>
+        <a href="?btn=6">6</a>
+        <a href="?btn=plus">+</a>
+
+
+        <a href="?btn=1">1</a>
+        <a href="?btn=2">2</a>
+        <a href="?btn=3">3</a>
+        <a href="?btn=minus">-</a>
+    </div>
+
+    <div class="btn_result">
+        <a href="?btn=enter">=</a>
+    </div>
+
+</body>
+
+</html>
